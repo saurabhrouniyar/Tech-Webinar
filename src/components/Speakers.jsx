b5ea5559039eb3ccda977d46c8c66c6d40f9cf92
saurabhrouniyar <saurabhkumar9123@gmail.com>
2025-12-15 18:18:49 +0530

    Fix dependencies and update Speakers component

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -11,7 +11,7 @@ const Speakers = () => {
         <div className="speaker-card">
           <div className="avatar bg-blue-500">R</div>
           <h3 className="text-lg font-semibold mt-4">Saurabh Kumar</h3>
-          <p className="text-gray-600">FrontEnd Developer</p>
+          <p className="text-gray-600">FrontEnd-Developer</p>
         </div>
 
         {/* Speaker 2 */}
